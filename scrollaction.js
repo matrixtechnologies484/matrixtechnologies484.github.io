9c19d22969efde75e8553b219d6939c1288e2b17
WinMithul <101418910+WinMithul@users.noreply.github.com>
2024-11-17 12:43:37 +0530

    Added more responsiveness on mobile devices

--- scrollaction.js
+++ scrollaction.js
@@ -1,4 +1,16 @@
 // JavaScript Document
+
+setInterval(get_height, 10);
+
+//window.onresize = function() {get_height()};
+
+function get_height()
+{
+	var uh = document.getElementById("navmenu");
+	var r = document.querySelector(':root');
+	r.style.setProperty('--ys',(uh.offsetHeight - 51)+"px");
+}
+
 function bar_check()
 {
   var r = document.querySelector(':root');
